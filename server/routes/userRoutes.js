de56e3830134aee88f70ad29c06f95aaa9e8a0eb
akanksh0301 <akankshachougule03@gmail.com>
2026-01-28 20:10:53 +0530

    Make community route public

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ import { auth } from "../middlewares/auth.js";
 const userRouter = express.Router();
 
 userRouter.get('/get-user-creations', auth, getUserCreations);
-userRouter.get('/get-published-creations', auth, getPublishedCreations);
+userRouter.get('/get-published-creations', getPublishedCreations);
 userRouter.post('/toggle-like-creation', auth, toggleLikeCreations);
 
 export default userRouter;
